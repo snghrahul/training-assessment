4c7f4b929c4292928e0e448a4ce96c7ef00529cf
snghrahul03 <snhrahul03@gmail.com>
2024-02-22 18:08:19 +0530

    added question no:10

--- core/src/main/java/com/training/aem/core/servlets/AlertContentFragmentServlet.java
+++ core/src/main/java/com/training/aem/core/servlets/AlertContentFragmentServlet.java
@@ -27,7 +27,6 @@ import java.util.List;
 
 })
 public class AlertContentFragmentServlet extends SlingSafeMethodsServlet {
-
     @Reference
     ContentFragmentService contentFragmentService;
     @Override
