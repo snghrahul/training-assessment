aea6107db2c4b599b3cf0e945a6fff976d59069f
snghrahul03 <snhrahul03@gmail.com>
2024-03-01 16:38:23 +0530

    removed commented code from SortedProductServiceImpl class

--- core/src/main/java/com/training/aem/core/services/impl/SortedProductServiceImpl.java
+++ core/src/main/java/com/training/aem/core/services/impl/SortedProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.training.aem.core.services.impl;
 
-
 import com.google.gson.Gson;
 import com.training.aem.core.bean.ProductDetailsEntity;
 import com.training.aem.core.services.SortedProductsService;
@@ -14,6 +13,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ import java.util.List;
 
 @Component(service = SortedProductsService.class)
 public class SortedProductServiceImpl implements SortedProductsService{
-
+    private static final Logger logger = LoggerFactory.getLogger(SortedProductServiceImpl.class);
     public List<ProductDetailsEntity> getAllProducts(String apiUrl){
         List<ProductDetailsEntity> productDetailsList = new ArrayList<>();
         try(CloseableHttpClient httpClient = HttpClients.createDefault()){
@@ -35,37 +36,15 @@ public class SortedProductServiceImpl implements SortedProductsService{
                 ProductDetailsEntity productDetailsEntity = new Gson().fromJson(jsonObject.toString(), ProductDetailsEntity.class);
                 productDetailsList.add(productDetailsEntity);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | JSONException exception) {
+            logger.error("Error occurred while fetching product details: " + exception.getMessage());
         }
-//        try {
-//            ClientResponse clientResponse = CommonUtils
-//                    .getClientResponse(CommonConstant.GET,apiUrl,null,null);
-//            if (clientResponse != null && clientResponse.getStatusCode() == HttpServletResponse.SC_OK){
-//                JSONArray jsonArray = new JSONArray();
-//                jsonArray.put(clientResponse.getData());
-//
-//                if(jsonArray!=null){
-//                    ObjectMapper objectMapper = new ObjectMapper();
-//                    productEntity = objectMapper.readValue(String.valueOf(jsonArray), new TypeReference<List<ProductDetailsEntity>>() {
-//                    });
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        return productEntity;
         return productDetailsList;
     }
     @Override
     public List<ProductDetailsEntity> getSortedProducts(String sortType) {
-
-
         List<ProductDetailsEntity> products = getAllProducts("https://fakestoreapi.com/products/");
         Collections.sort(products, new ProductDetailsEntityComparator(sortType));
-
         return products;
     }
 }
