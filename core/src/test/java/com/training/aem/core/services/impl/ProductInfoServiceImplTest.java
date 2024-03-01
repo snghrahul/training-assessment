4b47f65f3b25acdf64e13aa70aa0f8287b15791e
snghrahul03 <snhrahul03@gmail.com>
2024-03-01 18:02:08 +0530

    added test cases files

--- core/src/test/java/com/training/aem/core/services/impl/ProductInfoServiceImplTest.java
+++ core/src/test/java/com/training/aem/core/services/impl/ProductInfoServiceImplTest.java
@@ -1,8 +1,6 @@
 package com.training.aem.core.services.impl;
 
 import com.training.aem.core.bean.ProductDetailsEntity;
-import com.training.aem.core.services.PageCreationService;
-import com.training.aem.core.services.ProductService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,37 +13,29 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({AemContextExtension.class, MockitoExtension.class})
-class ApiServiceImplTest {
+class ProductInfoServiceImplTest {
     AemContext aemContext = new AemContext();
-    @Mock
-    PageCreationService pageCreationService;
     @Mock
     ProductServiceImpl productService;
     @InjectMocks
-    @Spy
-    ApiServiceImpl apiService;
-
+    ProductInfoServiceImpl productInfoService;
 
     @BeforeEach
     void setUp() {
-        aemContext.addModelsForClasses(ApiServiceImpl.class);
+        aemContext.addModelsForClasses(ProductInfoServiceImpl.class);
     }
 
     @Test
-    void fetchDataAndCreatePages() throws Exception {
+    void getProductInfo() throws Exception {
         ProductDetailsEntity productDetails = new ProductDetailsEntity();
         productDetails.setId(1);
-        productDetails.setPrice(900);
         productDetails.setCategory("clothing");
-        productDetails.setImage("google.com");
+        productDetails.setImage("google");
+        productDetails.setPrice(900);
         when(productService.getFakeApiData("https://fakestoreapi.com/products/" + "1")).thenReturn(productDetails);
-
-        apiService.fetchDataAndCreatePages();
-
-
+        productInfoService.getProductInfo("1");
     }
 }
